dd4cdd1c9336fead656fad5a8c193f90efce374f
fforw@gmx.de <fforw@gmx.de@b28efbb6-974e-0410-a7d0-456cb01d9dc4>
2008-12-17 11:06:09 +0000

    use new directory based design document updating
    git-svn-id: http://jcouchdb.googlecode.com/svn/trunk@64 b28efbb6-974e-0410-a7d0-456cb01d9dc4

--- src/org/jcouchdb/spring/SpringCouchDBUpdater.java
+++ src/org/jcouchdb/spring/SpringCouchDBUpdater.java
@@ -1,7 +1,12 @@
 package org.jcouchdb.spring;
 
+import java.io.IOException;
+
+import org.jcouchdb.db.Database;
 import org.jcouchdb.util.CouchDBUpdater;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.core.io.Resource;
 
 /**
  * Integrates the {@link CouchDBUpdater} into spring by making it an {@link InitializingBean}.
@@ -10,6 +15,18 @@ import org.springframework.beans.factory.InitializingBean;
  */
 public class SpringCouchDBUpdater extends CouchDBUpdater implements InitializingBean
 {
+    public void setDesignDocumentDir(Resource designDocumentDir) throws IOException
+    {
+        super.setDesignDocumentDir(designDocumentDir.getFile());
+    }
+
+    @Override
+    @Required
+    public void setDatabase(Database database)
+    {
+        super.setDatabase(database);
+    }
+
     public void afterPropertiesSet() throws Exception
     {
         updateDesignDocuments();
--- src/org/jcouchdb/util/CouchDBUpdater.java
+++ src/org/jcouchdb/util/CouchDBUpdater.java
@@ -1,23 +1,41 @@
 package org.jcouchdb.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.jcouchdb.db.Database;
 import org.jcouchdb.document.DesignDocument;
+import org.jcouchdb.document.View;
 import org.jcouchdb.exception.NotFoundException;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Required;
 
 public class CouchDBUpdater
 {
+    private static final String REDUCE_SUFFIX = ".reduce.js";
+
+    private static final String MAP_SUFFIX = ".map.js";
+
     protected static Logger log = Logger.getLogger(CouchDBUpdater.class);
 
     private Database database;
 
     private boolean createDatabase = true;
 
-    @Required
+    private File designDocumentDir;
+
+    public void setDesignDocumentDir(File designDocumentDir)
+    {
+        Assert.isTrue(designDocumentDir.exists(), "designDocumentDir must exist");
+        Assert.isTrue(designDocumentDir.isDirectory(), "designDocumentDir must actually be a directory");
+        this.designDocumentDir = designDocumentDir;
+    }
+
     public void setDatabase(Database database)
     {
         this.database = database;
@@ -26,14 +44,6 @@ public class CouchDBUpdater
     public void setCreateDatabase(boolean createDatabase)
     {
         this.createDatabase = createDatabase;
-    }
-
-    private List<DesignDocument> designDocuments;
-
-    @Required
-    public void setDesignDocuments(List<DesignDocument> designDocuments)
-    {
-        this.designDocuments = designDocuments;
     }
 
     private Database createOrGetDatabase()
@@ -60,11 +70,12 @@ public class CouchDBUpdater
         return database;
     }
 
-    public void updateDesignDocuments()
+    public List<DesignDocument> updateDesignDocuments() throws IOException
     {
         Database database = createOrGetDatabase();
 
-        for (DesignDocument designDocument : designDocuments)
+        List<DesignDocument> designDocuments = readDesignDocuments();
+        for (DesignDocument designDocument : designDocuments )
         {
             DesignDocument existing = null;
             try
@@ -90,5 +101,88 @@ public class CouchDBUpdater
                 database.createOrUpdateDocument(designDocument);
             }
         }
+        return designDocuments;
+    }
+
+    List<DesignDocument> readDesignDocuments() throws IOException
+    {
+        Assert.notNull(designDocumentDir, "designDocumentDir can't be null");
+
+        Collection<File> files = FileUtils.listFiles(designDocumentDir, new String[]{ "js"}, true);
+
+        Map<String,DesignDocument> designDocuments = new HashMap<String, DesignDocument>();
+
+        String designDocumentDirPath = designDocumentDir.getPath();
+
+        for (File file : files)
+        {
+            String path = file.getPath();
+            Assert.isTrue(path.startsWith(designDocumentDirPath), "not in dir");
+
+            path = path.substring(designDocumentDirPath.length());
+
+            boolean isMapFunction = path.endsWith(MAP_SUFFIX);
+            boolean isReduceFunction = path.endsWith(REDUCE_SUFFIX);
+            if (isMapFunction || isReduceFunction)
+            {
+                String content = FileUtils.readFileToString(file);
+
+                if (content == null || content.trim().length() == 0 )
+                {
+                    continue;
+                }
+
+                List<String> parts = StringUtil.split(path, File.separator);
+
+                Assert.isTrue(parts.size() > 1, "invalid dir structure");
+
+                String fnName = parts.remove(parts.size()-1);
+                String viewId = StringUtil.join(parts, "/");
+
+                DesignDocument designDocument = designDocuments.get(viewId);
+                if (designDocument == null)
+                {
+                    designDocument = new DesignDocument(viewId);
+                    designDocuments.put(viewId, designDocument);
+                }
+
+                if (isMapFunction)
+                {
+                    fnName = fnName.substring(0, fnName.length()-MAP_SUFFIX.length());
+                }
+                else
+                {
+                    fnName = fnName.substring(0, fnName.length()-REDUCE_SUFFIX.length());
+                }
+
+                View view = designDocument.getViews().get(fnName);
+                if (view == null)
+                {
+                    if (isMapFunction)
+                    {
+                        view = new View( content, null);
+                    }
+                    else
+                    {
+                        view = new View( null, content);
+                    }
+                    designDocument.getViews().put(fnName, view);
+                }
+                else
+                {
+                    if (isMapFunction)
+                    {
+                        view.setMap(content);
+                    }
+                    else
+                    {
+                        view.setReduce(content);
+                    }
+                }
+
+            }
+        }
+
+        return new ArrayList<DesignDocument>(designDocuments.values());
     }
 }
