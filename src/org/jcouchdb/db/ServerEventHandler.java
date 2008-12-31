263874d476d8aab94ab9a32cb4d2a56ec040d376
fforw@gmx.de <fforw@gmx.de@b28efbb6-974e-0410-a7d0-456cb01d9dc4>
2008-12-31 14:09:25 +0000

    git-svn-id: http://jcouchdb.googlecode.com/svn/trunk@88 b28efbb6-974e-0410-a7d0-456cb01d9dc4

--- src/org/jcouchdb/db/ServerEventHandler.java
+++
@@ -1,8 +0,0 @@
-package org.jcouchdb.db;
-
-public interface ServerEventHandler
-{
-    void executing(String method, String uri, Object data) throws Exception;
-
-    void executed(String method, String uri, Object data, Response response);
-}
