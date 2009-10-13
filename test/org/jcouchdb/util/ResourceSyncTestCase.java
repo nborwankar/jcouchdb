8f5741e3ff32be8091cecc225efc29f0020f5d3e
fforw@gmx.de <fforw@gmx.de@b28efbb6-974e-0410-a7d0-456cb01d9dc4>
2009-10-13 23:40:46 +0000

    final changes for 0.10.0-1
    git-svn-id: http://jcouchdb.googlecode.com/svn/trunk@188 b28efbb6-974e-0410-a7d0-456cb01d9dc4

--- test/org/jcouchdb/util/ResourceSyncTestCase.java
+++ test/org/jcouchdb/util/ResourceSyncTestCase.java
@@ -4,12 +4,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.jcouchdb.db.Database;
 import org.jcouchdb.db.LocalDatabaseTestCase;
 import org.jcouchdb.document.Attachment;
 import org.jcouchdb.document.BaseDocument;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +25,23 @@ public class ResourceSyncTestCase
 {
     private static Logger log = LoggerFactory
         .getLogger(ResourceSyncTestCase.class);
+    private File dir;
+    
+    @Before
+    public void init() throws IOException
+    {
+        dir = new File( System.getProperty("java.io.tmpdir") + File.separator + UUID.randomUUID().toString() );
+        dir.mkdirs();
+        dir.deleteOnExit();
+        
+        File sub = new File(dir, "sub");
+        sub.mkdir();
+        
+        FileUtils.writeStringToFile(new File(dir, "test.txt"), "123");
+        FileUtils.writeStringToFile(new File(sub, "test.txt"), "456");
+        
+    }
+    
     
     @Test
     public void test()
@@ -29,7 +52,7 @@ public class ResourceSyncTestCase
         LocalDatabaseTestCase.deleteDocIfExists(db, sync.getResourceBaseDocId());
         
         sync.setDatabase(db);
-        sync.setResourceBaseDir(new File("test/org/jcouchdb/util/resource-base/"));
+        sync.setResourceBaseDir(dir);
         sync.syncResources();
         
         BaseDocument doc = db.getDocument(BaseDocument.class, sync.getResourceBaseDocId());
